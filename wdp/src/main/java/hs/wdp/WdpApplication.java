a6f267f6ed79ce1b9dde2b8d16ab37d71f89f1a4
rtg <rtg2541@gmail.com>
2026-03-16 08:43:40 +0900

    [우아] 프로젝트 세팅

---
+++ wdp/src/main/java/hs/wdp/WdpApplication.java
@@ -0,0 +1,13 @@
+package hs.wdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WdpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WdpApplication.class, args);
+    }
+
+}
---
+++ wdp/src/test/java/hs/wdp/WdpApplicationTests.java
@@ -0,0 +1,13 @@
+package hs.wdp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WdpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
